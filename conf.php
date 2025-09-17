e27052799a9aee37f931dd9a4fc8da14cc2fa5f8
Munyoiks <167209005+Munyoiks@users.noreply.github.com>
2025-09-17 11:19:19 +0300

    Git Instructions Pt 1 & 2

---
+++ conf.php
@@ -0,0 +1,23 @@
+<?php
+// Site configuration
+$conf['site_name'] = "Munyoiks@Inc";
+$conf['site_email'] = "info@munyoiks.com";
+$conf['site_url'] = "http://localhost/Application-Programming-Internet/Plugins/PHPMailer/mail.php";
+
+// Site language
+$conf['language'] = "en";
+
+// Database constants
+$conf['db_type'] = "pdo";
+$conf['db_host'] = "localhost";
+$conf['db_user'] = "root";
+$conf['db_pass'] = "munyoiks7";
+$conf['db_name'] = "dol";
+
+// Email configuration
+$conf['mail_type'] = "smtp"; // Options: smtp, sendmail, mail
+$conf['smtp_host'] = "smtp.gmail.com";
+$conf['smtp_user'] = "ian.kariuki@strathmore.edu";
+$conf['smtp_pass'] = "ghds nxas ellw sbsy";
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = "ssl";
